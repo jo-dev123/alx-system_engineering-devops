7428afa55e35e28aa0295a121406c870be34fcae
jo-dev123 <johnsondegbor@gmail.com>
2022-05-31 21:00:54 -0700

    changing typo lesser to lesss

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,5 +13,5 @@ elif last_digit == 0:
 else:
     print(
             f"Last digit of {number} is {last_digit} and "
-            "is lesser than 6 and not 0"
+            "is less than 6 and not 0"
          )
