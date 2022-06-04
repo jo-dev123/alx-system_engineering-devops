e5ac0341aa719e9eef92ca8272364f68457f2b6b
jo-dev123 <johnsondegbor@gmail.com>
2022-06-04 05:19:08 -0700

    try to meet ob jective

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,6 @@ if __name__ == "__main__":
     from calculation_1 import add, sub, mul, div
     a = 10
     b = 5
-
     print("{:d} + {:d} = {:d}".format(a, b, add(a + b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a - b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a * b)))
