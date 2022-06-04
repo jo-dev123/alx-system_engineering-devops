64d22d603d4707e89581ad4c1d946c9917452ead
jo-dev123 <johnsondegbor@gmail.com>
2022-06-04 04:57:13 -0700

    using proper imported fuunction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,6 +5,6 @@
         b = 5
 
         print("{:d} + {:d} = {:d}".format(a, b, add(a + b)))
-        print("{:d} - {:d} = {:d}".format(a, b, add(a - b)))
-        print("{:d} * {:d} = {:d}".format(a, b, add(a * b)))
-        print("{:d} / {:d} = {:d}".format(a, b, add(a / b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a - b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a * b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a / b)))
