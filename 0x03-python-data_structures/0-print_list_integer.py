2bdd2b2d963d3d5767bb170a814a5b0102feb489
jo-dev123 <johnsondegbor@gmail.com>
2022-06-06 06:10:02 -0700

    remove excess in range

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 def print_list_integer(my_list=[]):
-    for i in range(0, len(my_list)):
+    for i in range(len(my_list)):
         if i < len(my_list):
             print("{:d}".format(my_list[i]))
     
