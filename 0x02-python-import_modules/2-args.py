d19952b40084d236db67c715bc92cf3bb50dd8e6
jo-dev123 <johnsondegbor@gmail.com>
2022-06-13 09:10:03 -0700

    changing main to __main__

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ = "main":
+if __name__ = "__main__":
     from sys import argv
     l = len(argv)
     print("{:d} {:s}{:s}".format(l - 1, "argument" if l <= 2 else "arguments", "." if l == 1 else ":"))
