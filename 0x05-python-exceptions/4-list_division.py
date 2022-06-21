75fb4b8c7a0770219988c192b302fba4559fca83
jo-dev123 <johnsondegbor@gmail.com>
2022-06-20 21:26:21 -0700

    swaping test ecept

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,12 +8,12 @@ def list_division(my_list_1, my_list_2, list_length):
         except (ValueError, TypeError):
             print("wrong type")
             result = 0
-        except IndexError:
-            print("out of range")
-            return = 0
         except ZeroDivisionError:
             print("division by 0")
             result = 0
+        except IndexError:
+            print("out of range")
+            return = 0
         finally:
             new_list.append(result)
     return new_list
