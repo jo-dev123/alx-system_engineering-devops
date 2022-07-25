bd3b352d139209556556e61f683c3aab2240ec0a
jo-dev123 <johnsondegbor@gmail.com>
2022-07-25 04:04:20 -0700

    adding 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""no imported modules here"""
+
+class Square:
+    """Does not do anything, no attribute"""
+
+    def  __size(self):
+        "Attribute for square size"
+        pass
+
+    pass
+
