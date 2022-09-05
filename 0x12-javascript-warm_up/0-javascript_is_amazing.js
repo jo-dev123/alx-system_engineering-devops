02617a79e9c67944895e6bdaebedb3c7d49a0289
jo-dev123 <johnsondegbor@gmail.com>
2022-09-05 10:49:55 -0700

    reduce length of comment

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// a constant variable called myVar with the value “JavaScript is amazing”
+// myVar with the value “JavaScript is amazing”
 
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
