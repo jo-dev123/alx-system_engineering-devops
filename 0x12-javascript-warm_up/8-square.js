673487433be57f7a3e17ae7414aac9dcb357946e
jo-dev123 <johnsondegbor@gmail.com>
2022-09-09 07:12:06 -0700

    converting to numbers

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const squareSize = process.argv[2];
+const squareSize = +process.argv[2];
 if (Number.isInteger(squareSize)) {
   for (let i = 0; i < squareSize; i++) {
     for (let j = 0; j < squareSize; j++) {
