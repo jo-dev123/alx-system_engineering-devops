2baab829f90f327026f4ebf8d3635a94604a72be
jo-dev123 <johnsondegbor@gmail.com>
2022-09-16 18:47:01 -0700

    c

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 # Using Puppet, create a file in /tmp
 
 file { 'sch':
-  path    => '/tmp/school',
   ensure  => 'file',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
